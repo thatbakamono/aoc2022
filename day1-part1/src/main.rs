114e88357b357d528d851b33a4f6f2b0d87d7ecf
Dorian <villainkernelerror@gmail.com>
2022-12-01 12:04:49 +0100

    Add solutions for day1

---
+++ day1-part1/src/main.rs
@@ -0,0 +1,25 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+fn main() {
+    let lines = BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .map(|line| line.unwrap())
+        .map(|line| line.parse::<usize>().ok());
+
+    let mut inventories = vec![];
+    let mut current_inventory = 0usize;
+
+    for line in lines {
+        if let Some(line) = line {
+            current_inventory += line;
+        } else {
+            inventories.push(current_inventory);
+            current_inventory = 0usize;
+        }
+    }
+
+    println!("{}", inventories.into_iter().max().unwrap());
+}
